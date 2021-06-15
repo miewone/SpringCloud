654b020fd570300ec837eb3a1bfa46eeb68f0fff
Miewone <dlsrk489@gmail.com>
2021-06-15 20:10:44 +0900

    Initial commit

---
+++ src/main/java/com/example/dnsapp/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.example.dnsapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hell()
+    {
+        return "long time no see";
+    }
+}
+
